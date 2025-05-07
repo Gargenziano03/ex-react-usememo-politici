5e486d94cf622a560a535c8cdb5a8c0b0b9a29dc
Gargenziano03 <gargenziano03@gmail.com>
2025-05-07 16:30:29 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PoliticiansList from './components/PoliticiansList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PoliticiansList />
+    </div>
+  );
+}
+
+export default App;
+
+
